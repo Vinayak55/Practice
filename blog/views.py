f2084583b94f370d078d4c668c44150b23c97da5
Vinayak55 <vinayakrk555@gmail.com>
2021-04-25 10:54:11 +0530

    first changes to template1

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def Post_details(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request,'base.html',{'posts':post})
+    return render(request,'base.html',{'posts':posts})
